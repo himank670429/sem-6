5b54480481acf12aae941fd52503f142ae9c4e6a
himanksingh <himanksingh44456@gmail.com>
2024-05-06 00:38:45 +0530

    added small message

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import data from "../assets/data";
 import SubjectSection from "../components/SubjectSection";
+import WarningIcon from '@mui/icons-material/Warning';
 function Home() {
 	return (
 		<>
+		<div className="bg-dark-50 p-4 mx-4 rounded-lg "><WarningIcon/> The playlist is incomplete is constanly updated by Himank. Thanks :)</div>
 			{data.subjects.map((item, index) => (
 				<SubjectSection {...item} key={index} index={index + 1} />
 			))}
